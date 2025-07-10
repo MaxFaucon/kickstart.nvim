2dd013145c6a130526ef45368ae5622f3f620b63
Maximilien Faucon <m.faucon@soilcapital.com>
2025-07-10 22:59:03 +0200

    change lualine to statusline

---
+++ lua/custom/plugins/statusline.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'beauwilliams/statusline.lua',
+    enabled = true,
+    dependencies = {
+      'nvim-lua/lsp-status.nvim',
+    },
+    config = function()
+      require('statusline').setup {
+        match_colorscheme = true, -- Enable colorscheme inheritance (Default: false)
+        tabline = false, -- Enable the tabline (Default: true)
+        lsp_diagnostics = true, -- Enable Native LSP diagnostics (Default: true)
+        ale_diagnostics = false, -- Enable ALE diagnostics (Default: false)
+      }
+    end,
+  },
+}
