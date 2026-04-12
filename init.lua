1f3e39dca1a593f5f5bbb8fa6513103a0f816bf0
Maximilien Faucon <maxth.faucon@gmail.com>
2026-04-12 15:24:22 +0200

    config: Move plugin folder to root

--- init.lua
+++ init.lua
@@ -9,25 +9,6 @@ require 'config.keymaps'
 -- Autocommands
 require 'config.autocommands'
 
--- [[ PLUGINS ]]
-require 'plugin.0_dependencies'
-require 'plugin.1_code'
-require 'plugin.2_file-explorer'
-require 'plugin.3_search'
-require 'plugin.4_ui'
-require 'plugin.5_completion'
-require 'plugin.6_lsp'
-require 'plugin.7_git'
-require 'plugin.8_ai'
-require 'plugin.9_database'
-require 'plugin.10_debug'
-require 'plugin.11_markdown'
-require 'plugin.12_terminal'
-require 'plugin.13_miscellaneous'
-require 'plugin.14_navigation'
-require 'plugin.15_testing'
-require 'plugin.16_tools'
-
 -- Custom plugins
 require 'lua.config.db.db'
 
